d1e721f29a9243b07fc29f200e91c2a1ca649315
Rui Baltazar <ruivrb@gmail.com>
2020-04-16 00:06:07 +0100

    sessions crud

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,52 @@
+class SessionsController < ApplicationController
+  before_action :fetch_session, only: %i[show edit update destroy]
+
+  def index
+    @sessions = Session.all
+  end
+
+  def show
+  end
+
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new(session_params)
+    if @session.save
+      redirect_to sessions_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @session.update(session_params)
+      redirect_to sessions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @session.destroy
+      redirect_to sessions_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def fetch_session
+    @session = Session.find(params[:id])
+  end
+
+  def session_params
+    params.require(:session).permit(:name, :start_date, :end_date)
+  end
+end
